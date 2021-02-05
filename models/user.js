c122bf4d0118e822e77ac3f058e14d495f47ebbb
eriBloo <19932449+EriBloo@users.noreply.github.com>
2021-02-05 11:59:07 +0100

    Added basic models

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true, maxlength: 30 },
+  password: { type: String, require: true },
+  rank: { type: Number, enum: [0, 1, 2] },
+});
+
+module.exports = mongoose.model('User', UserSchema);
