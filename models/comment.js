29ebcc71c9c587a8a655301e61b199501daa2c12
eriBloo <19932449+EriBloo@users.noreply.github.com>
2021-02-05 12:09:07 +0100

    Small styling changes

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  author: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  author: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true, maxlength: 300 },
 });
 
-module.epxorts = mongoose.model("Comment", CommentSchema);
+module.epxorts = mongoose.model('Comment', CommentSchema);
